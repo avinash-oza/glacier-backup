6350921064a0801eb91e45c7b4129f128862e34d
Avi Oza <avioza@gmail.com>
2020-01-02 20:45:39 -0500

    start converting to package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+        name='glacier-backup',
+        version='0.1b1',
+        packages=['glacier_backup',],
+        scripts=['bin/glacier_script.py'],
+        license='TBD',
+        long_description='TBD'
+        )
