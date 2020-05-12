31c246ce7ffd082ae90687651c242b4ef806fa3d
Avi Oza <avioza@gmail.com>
2020-05-11 20:14:47 -0400

    move file related logic in class

--- glacier_backup/file_data.py
+++ glacier_backup/file_data.py
@@ -12,11 +12,11 @@ import gnupg
 from boto3.s3.transfer import TransferConfig, MB
 from botocore.exceptions import ClientError
 
+logger = logging.getLogger(__name__)
+
 class FileData:
-    def __init__(self, folder_or_file_path, file_type, work_dir, listings_root_path):
+    def __init__(self, folder_or_file_path, work_dir, listings_root_path):
         self._file_path = folder_or_file_path
-        # TODO: check this
-        self._file_type = file_type
         self._work_dir = work_dir
         self._listings_root_path = listings_root_path
 
@@ -38,11 +38,14 @@ class FileData:
 
     @property
     def compressed_file_name(self):
-        if self._file_path.endswith('bz2') or self._file_path.endswith('gz'):
+        if self.compressed():
             # keep the compressed file as is
             return self.folder_name
 
         return '.'.join([self.folder_name, 'tar.gz'])
+
+    def compressed(self):
+        return self._file_path.endswith('bz2') or self._file_path.endswith('gz')
 
     @property
     def compressed_file_full_path(self):
@@ -64,20 +67,97 @@ class FileData:
     def storage_class(self):
         return 'DEEP_ARCHIVE' if self._file_type == 'photos' else 'GLACIER'
 
-    def compress(self, work_dir):
+    def compress(self):
         """
         Compresses the file in work_dir and returns the path to compressed file
-        :param work_dir:
+
         :return: str
         """
-        pass
+        logger.info("Start compressing")
 
-    def encrypt(self, file_path, work_dir, key_id):
+        if self.compressed():
+            logger.info("Not compressing as file is already compressed")
+            return self._file_path
+
+        # only tar when it is a directory and it doesnt exist
+        dest_tar_file_path = os.path.join(self._work_dir, self.compressed_file_name)
+        if not os.path.exists(dest_tar_file_path):
+            logger.info("Start tarring path: {}. Output path: {}".format(self._file_path, dest_tar_file_path))
+
+            with tarfile.open(dest_tar_file_path, 'w:gz') as tar:
+                tar.add(self._file_path)
+            logger.info("Finished path: {}. Output path: {}".format(self._file_path, dest_tar_file_path))
+
+        return dest_tar_file_path
+
+    def encrypt(self, file_path, key):
         """
         Encrypts the passed in file with key_id
-        :param key_id:
+        :param key:
         :param file_path:
         :param work_dir:
         :return: str of encrypted file
         """
-        pass
+
+        dest_file_name = '.'.join([file_path, 'gpg'])
+        dest_file_path = os.path.join(self._work_dir, dest_file_name)
+
+        if os.path.exists(dest_file_path):
+            logger.info("Found encrypted version of current file and will not rerun")
+            return dest_file_path
+
+        # Init GPG class
+        gpg = gnupg.GPG()
+        fingerprint = key['fingerprint']
+        logger.info("Fingerprint of key is {} and uid is {}".format(fingerprint, key['uids']))
+        logger.info("Start GPG encrypting path: {} Output path: {}".format(file_path, dest_file_path))
+
+        with open(file_path, 'rb') as tar_file:
+            ret = gpg.encrypt_file(tar_file, output=dest_file_path, armor=False, recipients=fingerprint)
+
+        logger.info("Encryption status: {} {} {}".format(ret.ok, ret.status, ret.stderr))
+
+        logger.info("Finished GPG encrypting path: {}  Output path: {}".format(file_path, dest_file_path))
+
+        return dest_file_path
+
+    def create_dir_listing(self, listing_dir):
+        """
+        Returns the directory listing
+
+        :return: file path or None
+        """
+
+        if not os.path.isdir(self._file_path):
+            logger.warning("Input is not a dir, not creating a dir listing")
+            return
+
+        listing_file_name = '.'.join([self.folder_name, 'gz'])
+        output_file_path = os.path.join(listing_dir, listing_file_name)
+
+        logger.info("Creating file containing the list of files {}".format(output_file_path))
+
+        with gzip.GzipFile(filename=output_file_path, mode='w') as gzipped_listing:
+            # with gzip.open(listing_file_path, 'wt') as f:
+            result = subprocess.run('du -ah {}'.format(self._file_path), shell=True, capture_output=True)
+            gzipped_listing.write(result.stdout)
+
+        logger.info("Done creating file {}".format(output_file_path))
+        return output_file_path
+
+# if __name__ == '__main__':
+#     import gnupg
+#
+#     gpg = gnupg.GPG()
+#     key_to_use = gpg.list_keys()[0]
+#     logging.basicConfig()
+#     logger.setLevel(logging.DEBUG)
+#
+#     # fd = FileData(r'/mnt/scratch/mta_test', 'a', 'a', 'a')
+#     fd = FileData(r'/mnt/scratch/mta_test', '/mnt/vm_drives', '/mnt/vm_drives')
+#
+#     # res = fd.compress(r'/mnt/vm_drives')
+#     # res = fd.encrypt(r'/mnt/vm_drives/mta_test.tar.gz', key_to_use)
+#     res = fd.create_dir_listing('/mnt/vm_drives')
+#
+#     logger.warning(res)
