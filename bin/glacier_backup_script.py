2009bfe9331044b1f0140ca0e7d43f8d24a593d5
Avi <avioza@gmail.com>
2025-04-21 20:22:53 -0400

    switch immich script to use click

--- bin/glacier_backup_script.py
+++ bin/glacier_backup_script.py
@@ -1,9 +1,12 @@
+import csv
+import datetime
 import logging
 import os
 
 import click
 
 from glacier_backup.backup_runner import BackupRunner
+from glacier_backup.file_data import UPLOAD_TIME_EVERY_BACKUP
 
 logging.basicConfig(
     level=logging.INFO,
@@ -15,6 +18,83 @@ logger = logging.getLogger(__name__)
 @click.group()
 def cli():
     pass
+
+
+@cli.command()
+@click.option(
+    "--immich-file-root",
+    type=str,
+    required=True,
+    help="Root of immich file installation",
+)
+@click.option(
+    "--output-file-path",
+    type=str,
+    required=True,
+    help="Path to write the output file",
+)
+@click.option(
+    "--full",
+    is_flag=True,
+    help="Do a full backup (vs current year only)",
+)
+def list_immich(immich_file_root, output_file_path, full_backup):
+    # thumbs -> complete directory every time
+    # upload -> complete directory every time
+    # library/1e958228-47fc-463d-83c7-0bc485a8cbfa/2024
+    output_list = []
+    output_list.extend(
+        [
+            (
+                os.path.join(immich_file_root, "photos", "thumbs"),
+                UPLOAD_TIME_EVERY_BACKUP,
+            ),
+            (
+                os.path.join(immich_file_root, "photos", "upload"),
+                UPLOAD_TIME_EVERY_BACKUP,
+            ),
+        ]
+    )
+
+    current_year = str(datetime.datetime.now().year)
+
+    photos_file_path = os.path.join(immich_file_root, "photos", "library")
+    for user in os.listdir(photos_file_path):
+        user_file_path = os.path.join(photos_file_path, user)
+
+        if not os.path.isdir(user_file_path):
+            logger.warning(f"Skipping {user_file_path=}")
+            continue
+
+        logger.info("Username: %s", user)
+
+        user_file_path = os.path.join(photos_file_path, user)
+        for year in os.listdir(user_file_path):
+            year_file_path = os.path.join(user_file_path, year)
+
+            archive_output_file_name = f"{user}__{year}"
+
+            if full_backup:
+                output_list.append(
+                    (year_file_path, UPLOAD_TIME_EVERY_BACKUP, archive_output_file_name)
+                )
+                continue
+
+            if year == current_year:
+                output_list.append(
+                    (year_file_path, UPLOAD_TIME_EVERY_BACKUP, archive_output_file_name)
+                )
+                logger.info("Setting current year to glacier")
+                continue
+            output_list.append(
+                (year_file_path, UPLOAD_TIME_EVERY_BACKUP, archive_output_file_name)
+            )
+
+    with open(output_file_path, "w") as f:
+        writer = csv.writer(f, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(["file_path", "upload_time", "output_file_path"])
+        for r in output_list:
+            writer.writerow(r)
 
 
 @cli.command()
